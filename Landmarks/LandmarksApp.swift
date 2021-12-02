ed5ff40061aaf17f7da12b2d8d00d1568417f57d
Abduqodir <qodirbekkhamzaev@gmail.com>
2021-12-02 16:10:29 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abduqodir's MacPro on 2021/12/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
